5797b075e9f7616791eb4ae5d074dea1d0b9b172
rajithst <rajiththennakoonshady@gmail.com>
2021-11-17 22:16:16 +0900

    bubble sort algorithm implementation

--- arrays/subarrya_sum.cpp
+++ arrays/subarrya_sum.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 
-int largestSubarraySum(int arr[],int n) {
+int largestSubarraySum(int arr[], int n) {
 
 	//prefix sum
 	int prefix[100] = {0};
 	prefix[0] = arr[0];
-	for (int i = 1; i < n; i++) { 
-		prefix[i] = prefix[i-1]+arr[i];
+	for (int i = 1; i < n; i++) {
+		prefix[i] = prefix[i - 1] + arr[i];
 	}
 
 	//subarray sum
 	int largestSum = 0;
-	for (int i = 0; i < n; i++){
-		for(int j=i; j< n; j++){
-			int subarraySum = i>0 ? prefix[j] - prefix[i-1] : prefix[j];
-			largestSum = max(subarraySum,largestSum);
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < n; j++) {
+			int subarraySum = i > 0 ? prefix[j] - prefix[i - 1] : prefix[j];
+			largestSum = max(subarraySum, largestSum);
 		}
 
 	}
@@ -26,8 +26,8 @@ int largestSubarraySum(int arr[],int n) {
 
 int main() {
 
-	int arr[] = {-2,3,4,-1,5,-12,6,1,3};
-	int n = sizeof(arr)/sizeof(int);
-	cout << largestSubarraySum(arr,n) << endl;
+	int arr[] = { -2, 3, 4, -1, 5, -12, 6, 1, 3};
+	int n = sizeof(arr) / sizeof(int);
+	cout << largestSubarraySum(arr, n) << endl;
 
 }
---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void bubble_sort(int arr[], int n) {
+
+	for (int times = 1; times <= n - 1; times++) {
+		for (int i = 0; i <= n - times - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				swap(arr[i], arr[i + 1]);
+			}
+		}
+	}
+}
+
+int main() {
+
+	int arr[] = { -2, 3, 4, -1, 5, -12, 6, 1, 3};
+	int n  = sizeof(arr) / sizeof(int);
+
+	bubble_sort(arr, n);
+	for(auto x:arr){
+		cout << x << endl;
+	}
+}

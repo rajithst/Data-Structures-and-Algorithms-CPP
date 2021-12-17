1cab5816078e9bf7a963840ca67627f08857d247
rajithst <rajiththennakoonshady@gmail.com>
2021-12-17 21:59:20 +0900

    binary search tree delete node method implementation

--- binary_search_tree/bst.cpp
+++ binary_search_tree/bst.cpp
@@ -33,6 +33,8 @@ bool search(Node *root,int key){
 		return true;
 	}
 }
+
+
 Node* insert(Node * root,int key) {
 
 	if(root == NULL){
@@ -45,6 +47,60 @@ Node* insert(Node * root,int key) {
 		root->right = insert(root->right,key);
 	}
 	return root;
+}
+
+Node* findMin(Node * root){
+	while(root->left!=NULL){
+		root = root->left;
+	}
+	return root;
+}
+
+
+Node* remove(Node * root,int key){
+
+	if (root == NULL)
+	{
+		return NULL;
+
+	}else if (key > root->key)
+	{
+		root->right = remove(root->right,key);
+
+	}else if (key < root->key)
+	{
+		root->left = remove(root->left,key);
+
+	}else{
+
+		if(root->left == NULL && root->right == NULL){ // if leaf node
+
+			delete root;
+			root = NULL;
+
+		}else if (root->left ==NULL) //which means root->right only exist
+		{
+			Node * temp = root;
+			root = root->right;
+			delete temp;
+
+		}else if (root->right == NULL) // which means root->left only exist
+		{
+			Node * temp = root;
+			root = root->left;
+			delete temp;
+
+		}else{						   // node has both left and right childs
+
+			//find in-order successor (which sitting in most left in right subtree)
+			Node * temp = findMin(root->right); //found in-order successor
+			root->key = temp->key; //copy in-order successor value to current node
+			root->right = remove(root->right,temp->key); //finally remove the inorder-successor node from right subtree of current node
+		}
+	}
+	return root;
+
+
 }
 void printInOrderTraversal(Node *root){
 	if(root == NULL){
@@ -64,5 +120,10 @@ int main(){
 		root = insert(root,x);
 	}
 	//printInOrderTraversal(root);
-	cout << search(root,11) << endl;
+	//cout << search(root,11) << endl;
+	root = remove(root,8);
+	printInOrderTraversal(root);
+	cout<<endl;
+	root = remove(root,6);
+	printInOrderTraversal(root);
 }
